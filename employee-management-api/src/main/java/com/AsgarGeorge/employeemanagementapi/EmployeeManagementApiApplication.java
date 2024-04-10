722af3e37832e10a1b6e6119f6d6973dabc24cec
Asgar_George <asgarantony@gmail.com>
2024-04-10 20:29:47 +0530

    first commit

---
+++ employee-management-api/src/main/java/com/AsgarGeorge/employeemanagementapi/EmployeeManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.AsgarGeorge.employeemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApiApplication.class, args);
+	}
+
+}
---
+++ employee-management-api/src/test/java/com/AsgarGeorge/employeemanagementapi/EmployeeManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AsgarGeorge.employeemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
