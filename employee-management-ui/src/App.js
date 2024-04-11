f47daf9bd4e1647774ebd20fd0463fdcf734b25e
Asgar_George <asgarantony@gmail.com>
2024-04-11 10:11:07 +0530

    adding frontend

---
+++ employee-management-ui/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './component/Navbar';
+import AddEmployee from './component/AddEmployee';
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <AddEmployee></AddEmployee>
+    </>
+  );
+}
+
+export default App;
---
+++ employee-management-ui/src/component/AddEmployee.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const AddEmployee =()=>{
+    return <div className="flex max-w-2xl mx-auto shadow border-b">
+        <div className="px-8 py-8">
+            <div className="font-thin text-2xl tracking-wider"><h1>Add New Employee</h1></div>
+            <div className="items-center justify-center h-14 w-full my-4">
+                <label className="block text-gray-600 text-sm font-normal">First Name</label>
+                <input type="text" className="h-10 w-96 border mt-2 px-2 py-2"></input>
+            </div>
+            <div className="items-center justify-center h-14 w-full my-4">
+                <label className="block text-gray-600 text-sm font-normal">Last Name</label>
+                <input type="text" className="h-10 w-96 border mt-2 px-2 py-2"></input>
+            </div>
+            <div className="items-center justify-center h-14 w-full my-4">
+                <label className="block text-gray-600 text-sm font-normal">Email ID</label>
+                <input type="text" className="h-10 w-96 border mt-2 px-2 py-2"></input>
+            </div>
+            <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
+                <button className="rounded text-white font-semibold bg-green-400 py-2 px-6 hover:bg-green-700">SAVE</button>
+                <button className="rounded text-white font-semibold bg-red-400 py-2 px-6 hover:bg-red-700">CLEAR</button>
+            </div>
+        </div>
+    </div>
+};
+
+export default AddEmployee;
